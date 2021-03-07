1532df3e8a006f73e25c90ee4fd99e82bc530a96
Matheus Lima <matheusmt31@gmail.com>
2021-03-07 00:24:08 -0300

    custom radio button and new activity

--- app/src/main/java/com/tcc/alif/MainActivity.kt
+++ app/src/main/java/com/tcc/alif/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.tcc.alif
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_login = findViewById<Button>(R.id.btn_login)
+
+        btn_login.setOnClickListener {
+            val intent = Intent(this, ModoActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
