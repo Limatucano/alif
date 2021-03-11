b14631aea888b1d114e18ad02dd7f9a7a13206d7
Matheus Lima <matheusmt31@gmail.com>
2021-03-11 00:12:53 -0300

    telas de apresentacao ao cadastrar

--- app/src/main/java/com/tcc/alif/Slides.kt
+++ app/src/main/java/com/tcc/alif/Slides.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
 
@@ -13,12 +14,19 @@ class Slides : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slides)
         val view_pager = findViewById<ViewPager>(R.id.view_pager)
-        view_pager.adapter =  OnBoardingAdapter()
+        val tips:Array<Tip> = arrayOf(
+            Tip("Tip1", R.color.pink),
+            Tip("Tip2", R.color.blue),
+            Tip("Tip3", R.color.black)
+        )
+        view_pager.adapter =  OnBoardingAdapter(tips)
     }
     private inner class OnBoardingAdapter(val tips: Array<Tip>) : PagerAdapter(){
         override fun instantiateItem(container: ViewGroup, position: Int): Any {
             val view = layoutInflater.inflate(R.layout.presentation, container, false)
             val tip_title = findViewById<TextView>(R.id.tip_title)
+            tip_title.text = tips[position].title
+            tip_title.setTextColor(ContextCompat.getColor(this@Slides, tips[position].color))
             container.addView(view)
             return view
         }
@@ -31,7 +39,7 @@ class Slides : AppCompatActivity() {
             return view == `object`
         }
 
-        override fun getCount(): Int = 3
+        override fun getCount(): Int = tips.size
 
 
 
---
+++ app/src/main/java/com/tcc/alif/Tip.kt
@@ -0,0 +1,6 @@
+package com.tcc.alif
+
+data class Tip(
+    val title:String,
+    val color: Int
+)
