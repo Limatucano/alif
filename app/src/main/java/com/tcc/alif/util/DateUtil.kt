5f795e20f85631a942ae7c51d69161bf0f9cde6a
Matheus Lima <matheusmt31@gmail.com>
2021-04-06 16:40:25 -0300

    dateUtil validar data

---
+++ app/src/main/java/com/tcc/alif/util/DateUtil.kt
@@ -0,0 +1,21 @@
+package com.tcc.alif.util
+
+import android.os.Build
+import java.util.*
+
+
+class DateUtil {
+    companion object{
+        fun myValidateDate(date : String) : Boolean {
+            val dateClean    = date.replace("/","")
+            if(dateClean.length != 8)
+                return false
+
+            var year_born = dateClean.substring(4,8).toLong()
+            val year      = Calendar.getInstance().get(Calendar.YEAR).toLong()
+            var total     = year - year_born
+
+            return total in 0..120
+        }
+    }
+}
