eb98ff845032e4da167701b0a2c0a3b334d1c600
Matheus Lima <matheusmt31@gmail.com>
2021-06-16 22:45:43 -0300

    @feat: add cliente view

---
+++ app/src/main/java/com/tcc/alif/view/ui/ClienteActivity.kt
@@ -0,0 +1,12 @@
+package com.tcc.alif.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tcc.alif.R
+
+class ClienteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cliente)
+    }
+}
