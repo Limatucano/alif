c92268ce5e78290a0ea50b393e9d16f87176f43b
Matheus Lima <matheusmt31@gmail.com>
2021-07-14 17:56:13 -0300

    timePicker

--- app/src/main/java/com/tcc/alif/FormFilaLojistaActivity.kt
+++ app/src/main/java/com/tcc/alif/FormFilaLojistaActivity.kt
@@ -2,10 +2,37 @@ package com.tcc.alif
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.util.Log
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.tcc.alif.databinding.ActivityFormFilaLojistaBinding
+import com.tcc.alif.model.util.TimerPickerHelper
+import java.util.*
 
-class FormFilaLojistaActivity : AppCompatActivity() {
+class FormFilaLojistaActivity : AppCompatActivity(){
+    private val viewBinding : ActivityFormFilaLojistaBinding by viewBinding()
+    lateinit var timePicker : TimerPickerHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_fila_lojista)
+        timePicker = TimerPickerHelper(this, true, true)
+        viewBinding.horarioAbertura.inputType = InputType.TYPE_NULL
+        viewBinding.calendarHorarioAbertura.setOnClickListener {
+            showTimePickerDialog()
+        }
+    }
+    private fun showTimePickerDialog(){
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+
+        timePicker.showDialog(hour, minute, object: TimerPickerHelper.Callback{
+            override fun onTimeSelected(hourOfDay: Int, minute: Int) {
+                val hourStr = hourOfDay.toString().padStart(2,'0')
+                val minuteStr = minute.toString().padStart(2,'0')
+                val formatted = "${hourStr}:${minuteStr}"
+                viewBinding.horarioAbertura.setText(formatted)
+            }
+        })
     }
 }
