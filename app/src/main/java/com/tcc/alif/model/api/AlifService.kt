05ea8cac548a0300df4e1ad64baa31e77cc2f93b
Matheus Lima <matheusmt31@gmail.com>
2021-07-25 00:41:16 -0300

    realizando o delete de fila

--- app/src/main/java/com/tcc/alif/model/api/AlifService.kt
+++ app/src/main/java/com/tcc/alif/model/api/AlifService.kt
@@ -5,8 +5,17 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.DELETE
+import retrofit2.http.Path
 
 interface AlifService {
+
+    //Deletar fila
+    @Headers("Content-Type: application/json")
+    @DELETE("fila/{id_fila}")
+    fun deleteFila(
+        @Path("id_fila") id_fila: String
+    ): Call<MessageRequest>
 
     //Pegar filas do lojista
     @Headers("Content-Type: application/json")
