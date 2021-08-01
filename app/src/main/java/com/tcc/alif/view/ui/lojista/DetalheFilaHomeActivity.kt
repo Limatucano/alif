0b1d7796736abfca3c55aa931029b88eea34406e
Matheus Lima <matheusmt31@gmail.com>
2021-07-31 23:21:29 -0300

    iniciando layout detalhe fila home

---
+++ app/src/main/java/com/tcc/alif/view/ui/lojista/DetalheFilaHomeActivity.kt
@@ -0,0 +1,12 @@
+package com.tcc.alif.view.ui.lojista
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tcc.alif.R
+
+class DetalheFilaHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhe_fila_home)
+    }
+}
