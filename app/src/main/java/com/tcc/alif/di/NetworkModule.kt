3849e67c59db78dae8a7b792baa4775590d42ea6
matheus lima <matheusmt31@gmail.com>
2022-08-05 08:56:17 -0300

    feat: added logging inteceptor in module

--- app/src/main/java/com/tcc/alif/di/NetworkModule.kt
+++ app/src/main/java/com/tcc/alif/di/NetworkModule.kt
@@ -1,14 +1,24 @@
 package com.tcc.alif.di
 
+import android.app.Application
+import android.content.Context
 import com.tcc.alif.data.api.AlifService
 import com.tcc.alif.data.util.Constants.API_BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
 import javax.inject.Singleton
+
+private const val READ_TIMEOUT = 30L
+private const val CONNECT_TIMEOUT = 30L
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -19,14 +29,44 @@ object NetworkModule {
     fun provideConverterFactory(): GsonConverterFactory {
         return GsonConverterFactory.create()
     }
+
+    @Singleton
+    @Provides
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    @Singleton
+    @Provides
+    fun providesHttpInterceptor(): Interceptor {
+        return Interceptor {
+            val request = it.request().newBuilder()
+            val actualRequest = request.build()
+            it.proceed(actualRequest)
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+            .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+            .addInterceptor(providesHttpInterceptor())
+            .addInterceptor(providesLoggingInterceptor())
+            .build()
+    }
+
     @Singleton
     @Provides
     fun provideRetrofitInstance(
         gsonConverterFactory: GsonConverterFactory,
+        okHttpClient: OkHttpClient
     ): Retrofit {
         return Retrofit.Builder()
             .baseUrl(API_BASE_URL)
             .addConverterFactory(gsonConverterFactory)
+            .client(okHttpClient)
             .build()
     }
 
@@ -34,5 +74,12 @@ object NetworkModule {
     @Provides
     fun provideMovieApiService(retrofit: Retrofit): AlifService {
         return retrofit.create(AlifService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    @Named("AppContext")
+    fun provideContext(application: Application): Context? {
+        return application.applicationContext
     }
 }
